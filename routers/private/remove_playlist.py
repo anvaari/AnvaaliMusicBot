4624118248649229c23cc198c8b496fee24e74f5
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-08-12 21:20:55 +0330

    Add route for remove_playlist

---
+++ routers/private/remove_playlist.py
@@ -0,0 +1,69 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+from aiogram.fsm.context import FSMContext
+import services.playlist_service as ps
+from states.user import PlaylistStates
+from utils.logging import get_logger
+from utils.typing import (
+    get_user_id,
+    get_callback_text_safe,
+    get_callback_message,
+    get_edit_markup_message,
+    get_edit_text_message
+)
+from keyboards.inline import get_playlist_delete_confirmation_keyboard
+
+logger = get_logger(__name__)
+
+remove_playlist_router = Router()
+
+@remove_playlist_router.callback_query(F.data.startswith("delete_playlist:"))
+async def delete_playlist_handler(callback: CallbackQuery):
+    callback_text = get_callback_text_safe(callback)
+    callback_message = get_callback_message(callback)
+
+    playlist_name = callback_text.split(":")[1]
+    
+    await callback.answer(
+        text=f"Are you sure you want to delete '{playlist_name}'?\nClick again to confirm.",
+        show_alert=True
+    )
+    kb = get_playlist_delete_confirmation_keyboard(playlist_name)
+
+    edit_markup_message = get_edit_markup_message(callback_message)
+    return await edit_markup_message(reply_markup=kb)
+
+
+@remove_playlist_router.callback_query(F.data.startswith("confirm_delete:"))
+async def delete_confirm_action(callback: CallbackQuery):
+    callback_text = get_callback_text_safe(callback)
+    callback_message = get_callback_message(callback)
+    
+    playlist_name = callback_text.split(":")[1]
+
+    user_id = get_user_id(callback)
+    user_db_id = ps.get_user_id(user_id)
+
+    edit_text_message = get_edit_text_message(callback_message)
+
+    success = ps.delete_playlist(user_db_id, playlist_name)
+    if success:
+        logger.info(f"User {user_id} deleted playlist '{playlist_name}'")
+        await edit_text_message(f"🗑 Playlist '{playlist_name}' deleted.")
+        return await callback.answer()
+
+    else:
+        logger.warning(f"User {user_id} failed to delete playlist '{playlist_name}'")
+        await edit_text_message(f"❌ Failed to delete *{playlist_name}*.")
+        return await callback.answer()
+
+        
+@remove_playlist_router.callback_query(F.data == "cancel_delete")
+async def delete_cancel_action(callback: CallbackQuery):
+    callback_message = get_callback_message(callback)
+    edit_text_message = get_edit_text_message(callback_message)
+
+    await edit_text_message("❌ Deletion canceled.")
+    await callback.answer()
+
+
