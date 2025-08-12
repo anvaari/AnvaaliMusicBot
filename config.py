653ead81ffce054e22cff2856a9ce1f27d1070a0
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-08-12 10:40:01 +0330

    Add project_root_dir and time_window_for_add_track vars

--- config.py
+++ config.py
@@ -11,7 +11,9 @@ class appConfigs:
     BOT_TOKEN: str = getenv("BOT_TOKEN","")
     LOG_LEVEL: str = getenv("LOG_LEVEL","INFO")
     DATABASE_NAME: str = getenv("DATABASE_NAME","playlist.db")
-    PROJECT_ROOT_DIR: str = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute())
+    PROJECT_ROOT_DIR: str = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).absolute())
+    # Max delay between text and audio forwards (in seconds)
+    ADD_TRACK_TIME_WINDOW: int = int(getenv("ADD_TRACK_TIME_WINDOW","60"))
 
     def __post_init__(self):
         if not self.BOT_TOKEN:
