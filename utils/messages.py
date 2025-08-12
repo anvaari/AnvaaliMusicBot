dd6dfa3d43364b60992d373c7688c78c8b509890
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-08-12 10:53:41 +0330

    Add messages.py
    tools for work and manipulate messages

---
+++ utils/messages.py
@@ -0,0 +1,34 @@
+import re
+
+from aiogram.types import Message
+
+
+def is_text_starts_with_emoji(text: str) -> bool:
+    """Check if text starts with an emoji"""
+    if not text:
+        return False
+    # Unicode ranges for emojis
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\U00002500-\U00002BEF"  # chinese char
+        "\U00002702-\U000027B0"
+        "\U00002702-\U000027B0"
+        "\U000024C2-\U0001F251"
+        "\U0001f926-\U0001f937"
+        "\U00010000-\U0010ffff"
+        "\u2640-\u2642"
+        "\u2600-\u2B55"
+        "\u200d"
+        "\u23cf"
+        "\u23e9"
+        "\u231a"
+        "\ufe0f"  # dingbats
+        "\u3030"
+        "]+", flags=re.UNICODE
+    )
+    return bool(emoji_pattern.match(text.strip()))
+
