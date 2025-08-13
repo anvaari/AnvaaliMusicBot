6054cd603cf6f274597ad7a4ae7885d9e25a5f73
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-08-13 22:24:54 +0330

    Create mapping of emojies and filter them instead of regex

--- utils/messages.py
+++ utils/messages.py
@@ -1,34 +1,26 @@
-import re
+from enum import Enum
 
-from aiogram.types import Message
+class EMOJIS(str, Enum):
+    HEADPHONE = '🎧'
+    ADD = '➕'
+    MIC = '🎤'
+    TRASH = '🗑'
+    CHECK_MARK = '✅'
+    FAIL = '❌'
+    LIST = '📋'
+    MUSIC = '🎵'
+    CAMERA = '📸'
+    LINK = '🔗'
+    FILE = '📂'
+    LIST_WITH_PEN = '📝'
+    PHOTO = '🖼️'
+    NEW = '🆕'
+    DANGER = '☠'
 
 
 def is_text_starts_with_emoji(text: str) -> bool:
-    """Check if text starts with an emoji"""
+    """Check if text starts with a UI command emoji."""
+    text = text.strip()
     if not text:
         return False
-    # Unicode ranges for emojis
-    emoji_pattern = re.compile(
-        "["
-        "\U0001F600-\U0001F64F"  # emoticons
-        "\U0001F300-\U0001F5FF"  # symbols & pictographs
-        "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U00002500-\U00002BEF"  # chinese char
-        "\U00002702-\U000027B0"
-        "\U00002702-\U000027B0"
-        "\U000024C2-\U0001F251"
-        "\U0001f926-\U0001f937"
-        "\U00010000-\U0010ffff"
-        "\u2640-\u2642"
-        "\u2600-\u2B55"
-        "\u200d"
-        "\u23cf"
-        "\u23e9"
-        "\u231a"
-        "\ufe0f"  # dingbats
-        "\u3030"
-        "]+", flags=re.UNICODE
-    )
-    return bool(emoji_pattern.match(text.strip()))
-
+    return text[0] in EMOJIS._value2member_map_
