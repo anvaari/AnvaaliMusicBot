555ca9be65e1de0bb2a0da6dd5a2ba213634917f
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2025-08-13 22:17:18 +0330

    Add not null to ddl

--- database/db.py
+++ database/db.py
@@ -19,20 +19,20 @@ def init_db():
             cur= conn.cursor()
             cur.execute("""CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                telegram_id INTEGER UNIQUE
+                telegram_id INTEGER NOT NULL UNIQUE
             )""")
             cur.execute("""CREATE TABLE IF NOT EXISTS playlists (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER,
-                name TEXT,
+                user_id INTEGER NOT NULL,
+                name TEXT NOT NULL,
                 cover_file_id TEXT,
                 UNIQUE(user_id, name),
                 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
             )""")
             cur.execute("""CREATE TABLE IF NOT EXISTS tracks (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                playlist_id INTEGER,
-                file_id TEXT,
+                playlist_id INTEGER NOT NULL,
+                file_id TEXT NOT NULL,
                 UNIQUE(playlist_id,file_id),
                 FOREIGN KEY (playlist_id) REFERENCES playlists (id) ON DELETE CASCADE
             )""")
